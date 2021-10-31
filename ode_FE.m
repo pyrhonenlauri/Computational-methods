dbc5181fdc904d291f9fff0dac83c22147a36697
User <User@DESKTOP-4TCUK0N>
2021-10-31 18:13:47 +0200

    Function created

---
+++ ode_FE.m
@@ -0,0 +1,11 @@
+function [sol, time] = ode_FE(f, U_0, dt, T)
+    N_t = floor(T/dt);
+    u = zeros(N_t+1, 1);
+    t = linspace(0, N_t*dt, length(u));
+    u(1) = U_0;
+    for n = 1:N_t
+        u(n+1) = u(n) + dt*f(u(n), t(n));
+    end
+    sol = u;
+    time = t;
+end
