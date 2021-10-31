46cf9162b2acf6c96b78eabf5dbe43a1bc98b7b1
User <User@DESKTOP-4TCUK0N>
2021-10-31 18:43:48 +0200

    File ready and working. Comments to be added.

--- logistic_dt.m
+++ logistic_dt.m
@@ -1,7 +1,29 @@
 %% Exercise 4.4
+close all
+clear all
 f = @(u, t) 0.1*(1 - u/500)*u;
 U_0 = 100;
-dt = 0.5; T = 60;
-[u, t] = ode_FE(f, U_0, dt, T);
-plot(t, u, 'b-');
-xlabel('t'); ylabel('N(t)');
+dt = 10;
+T = 60;
+[u_prev, t_prev] = ode_FE(f, U_0, dt, T);
+dt=dt/2;
+while 1
+    [u, t] = ode_FE(f, U_0, dt, T);
+    %%plot
+    plot(t_prev, u_prev, 'b-');
+    hold on
+    plot(t,u,'r--')
+    xlabel('t'); ylabel('N(t)');
+    lgd=legend(['dt=' num2str(dt)],['dt=' num2str(2*dt)]);
+    lgd.Location='northwest';
+    %%user input
+    str = input('If you want to calculate again, press y. If you want to quit, press q.','s');
+    if str=='y'
+        dt=dt/2;
+        t_prev=t;
+        u_prev=u;
+        close all
+    else
+        return
+    end
+end
